0e2e12953f8236a6404c171a05456102c7ac26c4
Wiebke Hagenauer <wiebosaurus@hotmail.com>
2022-08-24 15:38:51 +0200

    Lab done

--- src/viking.js
+++ src/viking.js
@@ -36,7 +36,7 @@ class Saxon extends Soldier {
   receiveDamage(damage) {
     this.health -= damage;
     if (this.health > 0) {
-      return `A Saxon has received DAMAGE ${damage} points of damage`;
+      return `A Saxon has received ${damage} points of damage`;
     } else {
       return `A Saxon has died in combat`;
     }
@@ -53,23 +53,28 @@ class War {
   addViking(Viking) {
     this.vikingArmy.push(Viking);
   }
-
   addSaxon(Saxon) {
     this.saxonArmy.push(Saxon);
   }
 
   vikingAttack() {
-    const goViking = Saxon.receiveDamage(damage){
-      damage = Viking.strength
-    }
+    const goViking = Saxon.receiveDamage(damage);
+    this.damage = Viking.strength;
 
-    if(Saxon.health <= 0){
-      this.saxonArmy.pop(Saxon)
+    if (Saxon.health <= 0) {
+      this.saxonArmy.pop(Saxon);
     }
-
     return goViking;
   }
 
   saxonAttack() {}
-  showStatus() {}
+  showStatus() {
+    if (this.saxonArmy === 0) {
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy === 0) {
+      return "Saxons have fought for their lives and survived another day...";
+    } else if (this.saxonArmy <= 1 && this.vikingArmy <= 1) {
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  }
 }
